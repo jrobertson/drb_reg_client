06c5ebb9f2fededfe36e890a5ef6b4c99b7742da
Your Name <you@example.com>
2017-01-02 00:02:58 +0000

    1st commit

---
+++ drb_reg_client.gemspec
@@ -0,0 +1,13 @@
+Gem::Specification.new do |s|
+  s.name = 'drb_reg_client'
+  s.version = '0.1.0'
+  s.summary = 'Provides a DWS_registry service using DRb; Requires a drb_reg_server to be running.'
+  s.authors = ['James Robertson']
+  s.files = Dir['lib/drb_reg_client.rb']
+  s.add_runtime_dependency('rexle', '~> 1.4', '>=1.4.3')
+  s.signing_key = '../privatekeys/drb_reg_client.pem'
+  s.cert_chain  = ['gem-public_cert.pem']
+  s.license = 'MIT'
+  s.email = 'james@jamesrobertson.eu'
+  s.homepage = 'https://github.com/jrobertson/drb_reg_client'
+end
---
+++ lib/drb_reg_client.rb
@@ -0,0 +1,96 @@
+#!/usr/bin/env ruby
+
+# file: drb_reg_client.rb
+
+require 'drb'
+require 'json'
+require 'rexle'
+
+
+class DRbRegClient
+
+
+  def initialize(host: 'localhost', port: '59500')
+    DRb.start_service
+
+    # attach to the DRb server via a URI given on the command line
+    @reg = DRbObject.new nil, "druby://#{host}:#{port}"
+  end
+
+  def delete_key(path)
+    @reg.delete_key path
+  end
+
+  def get_key(key='', auto_detect_type: false)
+
+    r = @reg.get_key(key, auto_detect_type: auto_detect_type)     
+
+    return unless r
+
+    doc = Rexle.new(r)
+    e = doc.root
+    
+    return e unless auto_detect_type
+    
+    c = e.attributes[:type]
+    s = e.text
+
+    return e if e.elements.length > 0 or s.nil?    
+    return s unless c
+          
+    h = {
+      string: ->(x) {x},
+      boolean: ->(x){ 
+        case x
+        when 'true' then true
+        when 'false' then false
+        when 'on' then true
+        when 'off' then false
+        else x
+        end
+      },
+      number: ->(x){  x[/^[0-9]+$/] ? x.to_i : x.to_f },
+      time:   ->(x) {Time.parse x},
+      json:   ->(x) {JSON.parse x}
+    }
+                            
+    h[c.to_sym].call s         
+
+  end
+    
+
+  def get_keys(path)
+    recordset = @reg.get_keys(key)
+
+    if recordset then
+      recordset.to_doc(root: 'recordset').root.xml
+    else
+      nil
+    end    
+  end
+
+  def set_key(key, value)
+
+    r = @reg.set_key(key, value)
+    return unless r
+
+    doc = Rexle.new(r)
+    doc.root
+  end
+
+  def xpath(path)
+    
+    r = @reg.xpath path
+
+    return [] if r.empty?
+
+    doc = Rexle.new(r)
+    
+    if doc.root.elements then
+      doc.root.elements.to_a 
+    else
+      []
+    end
+  end
+
+end 
